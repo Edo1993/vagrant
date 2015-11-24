38d70bcfb3c095528020c81f053fd6f07124833e
Attila Kanto <attila.kanto@gmail.com>
2015-11-24 09:12:55 +0100

    Add promiscous mode

--- ovs-demo/demo.sh
+++ ovs-demo/demo.sh
@@ -6,7 +6,7 @@ ovs-vsctl show
 ovs-vsctl add-port br0 eth1
 ifconfig eth1 0.0.0.0
 
-dhclient br0
+#dhclient br0
 
 #ip tuntap add mode tap vport1
 #ip tuntap add mode tap vport2
@@ -23,8 +23,14 @@ ip netns add ns1
 ip link add tap1 type veth peer name ovs-tap1
 # attach one side to ovs
 ovs-vsctl add-port br0 ovs-tap1
+
+# To show the links
+# ip link list
 # attach the other side to namespace
 ip link set tap1 netns ns1
+# Chek again, and inspect that teh tap1 disappeared
+# ip link list
+
 # set the ports to up
 ip netns exec ns1 ip link set dev lo up
 ip netns exec ns1 ip link set dev tap1 up
