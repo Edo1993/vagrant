a2fda777313ca647b19eb9a55619e87a268061a2
Attila Kanto <attila.kanto@gmail.com>
2015-11-24 08:31:41 +0100

    Add OVS test environment

---
+++ ovs-demo/demo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ovs-vsctl add-br br0
+ovs-vsctl show
+
+ovs-vsctl add-port br0 eth1
+ifconfig eth1 0.0.0.0
+
+dhclient br0
+
+#ip tuntap add mode tap vport1
+#ip tuntap add mode tap vport2
+#ifconfig vport1 up
+#ifconfig vport2 up
+
+#ovs-vsctl add-port br0 vport1
+#ovs-vsctl add-port br0 vport2
+
+# add the namespaces
+ip netns add ns1
+#### PORT 1 https://github.com/dave-tucker/container-connection-comparison/blob/master/ovs-veth.sh
+# create a port pair
+ip link add tap1 type veth peer name ovs-tap1
+# attach one side to ovs
+ovs-vsctl add-port br0 ovs-tap1
+# attach the other side to namespace
+ip link set tap1 netns ns1
+# set the ports to up
+ip netns exec ns1 ip link set dev lo up
+ip netns exec ns1 ip link set dev tap1 up
+ip link set dev ovs-tap1 up
+#
+ip netns exec ns1 dhclient tap1
+
+ip netns exec ns1 ifconfig
+
+ovs-vsctl show
